15f5e6dd8ab473ad9806887c4653cd893fba787d
Yeasin <yeasin30112001@gmail.com>
2021-02-06 15:40:35 -0600

    add country details with template string

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ fetch('https://restcountries.eu/rest/v2/all')
 .then(data => getCountriesData(data))
 
 function getCountriesData(data){
-    // console.log(data);
+    console.log(data);
     const countries = document.getElementById('country-container');
     for (let i = 0; i < data.length; i++) {
         const country = data[i];
-        // console.log(country.name);
         const countryDiv = document.createElement('div');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        countryDiv.appendChild(h3);
-        countryDiv.appendChild(p);
+
+        const countryInfo = `
+            <h3>${country.name}</h3>
+            <p>${country.capital}</p>
+        `
+        countryDiv.innerHTML = countryInfo;
         countries.appendChild(countryDiv);
+        
         
         
     }
