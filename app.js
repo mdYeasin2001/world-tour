456dff9865928f9a317feb9d0543a9db1006984d
Yeasin <yeasin30112001@gmail.com>
2021-02-06 15:52:23 -0600

    eccess country with forEach() and add some style

--- app.js
+++ app.js
@@ -5,18 +5,16 @@ fetch('https://restcountries.eu/rest/v2/all')
 function getCountriesData(data){
     console.log(data);
     const countries = document.getElementById('country-container');
-    for (let i = 0; i < data.length; i++) {
-        const country = data[i];
-        const countryDiv = document.createElement('div');
-
+    data.forEach(country => {
+        // console.log(country);
         const countryInfo = `
-            <h3>${country.name}</h3>
+            <h3 class="country-name">${country.name}</h3>
             <p>${country.capital}</p>
         `
+        const countryDiv = document.createElement('div');
+        countryDiv.className = 'country';
         countryDiv.innerHTML = countryInfo;
         countries.appendChild(countryDiv);
-        
-        
-        
-    }
+    });
+    
 }
